792d9b55e62a2213d6e278ba5d2bdebb70019530
Sorayukii <sorayukii69@gmail.com>
2026-04-03 17:54:37 +0700

    akari: Welcome ArrowOS-ext
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_akari.mk
+    $(LOCAL_DIR)/arrow_akari.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_akari-user \
-    lineage_akari-userdebug \
-    lineage_akari-eng
+    arrow_akari-user \
+    arrow_akari-userdebug \
+    arrow_akari-eng
